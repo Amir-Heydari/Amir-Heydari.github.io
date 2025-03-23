62430d67fe31363e4eceb36a3fa9fe128db44a7b
Amir-Heydari <amirsystemomen@gmail.com>
2025-03-24 02:06:32 +0330

    build issues fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,6 @@ export default function Home() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      smooth: true,
     })
 
     function raf(time: number) {
