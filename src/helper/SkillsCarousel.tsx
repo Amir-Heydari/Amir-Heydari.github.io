2d53dd2bb99b5d906170f6fef1cc6f03d173e4c2
Amir-Heydari <amirsystemomen@gmail.com>
2025-03-25 16:46:50 +0330

    skills carousel bg fixed

--- src/helper/SkillsCarousel.tsx
+++ src/helper/SkillsCarousel.tsx
@@ -83,7 +83,7 @@ const SkillsCarousel = () => {
     };
 
     return (
-        <div className="w-full py-16 px-4 md:px-8 bg-background text-primary">
+        <div className="w-full py-16 px-4 md:px-8 bg-transparent text-primary">
             <div className="max-w-2xl mx-auto">
                 <div className="mb-16 text-center">
                     <h2 className="text-3xl md:text-4xl font-bold mb-4">My Skills</h2>
